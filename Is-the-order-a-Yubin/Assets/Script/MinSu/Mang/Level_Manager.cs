3346ebe4503b7d4658fce8aa0dcc107f74bbc0ba
the-glory-of-the-Sun <aenigo23gi@naver.com>
2024-01-15 21:24:19 +0900

    2401김민수
    .

--- Is-the-order-a-Yubin/Assets/Script/MinSu/Mang/Level_Manager.cs
+++ Is-the-order-a-Yubin/Assets/Script/MinSu/Mang/Level_Manager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Experience_Bar : MonoBehaviour
+public class Level_Manager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
