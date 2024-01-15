23fef7fc8cb5800042466f9a082d6f291435dadd
sheepyubin <yangyubinstory@naver.con>
2024-01-15 19:26:32 +0900

    Revert "Revert "Dragon Move""
    This reverts commit 4c838f64676d0592552448a0c684583d3ca2ab3f.

--- Is-the-order-a-Yubin/Assets/Script/DarkDragon/MoveScript.cs
+++ Is-the-order-a-Yubin/Assets/Script/DarkDragon/MoveScript.cs
@@ -1,19 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveScript : MonoBehaviour
 {
-    public float speed;
+    public float speed = 20;
+
+    Rigidbody rigidbody;
+    Vector3 movement;
+
+    public int maxHp;
+    public int nowHp;
+    public int atkDmg;
+    public float atkSpeed = 1;
+    public bool attacked = false;
+    public Image nowHpbar;
+
+    void AttackTrue()
+    {
+        attacked = true;
+    }
+    void AttackFalse()
+    {
+        attacked = false;
+    }
+    void SetAttackSpeed(float speed)
+    {
+        //animator.SetFloat("attackSpeed", speed);
+        atkSpeed = speed;
+    }
 
     void Start()
     {
-
+        maxHp = 50;
+        nowHp = 50;
+        atkDmg = 10;
+        //animator = GetComponent<Animator>;
+        SetAttackSpeed(1.5f);
+        rigidbody = GetComponent<Rigidbody>();
     }
-
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        nowHpbar.fillAmount = (float)nowHp / (float)maxHp;
+        if (Input.GetKey(KeyCode.W))
         {
             transform.position = transform.position + transform.up * Time.deltaTime * speed;
         }
@@ -28,6 +58,10 @@ public class MoveScript : MonoBehaviour
         else if (Input.GetKey(KeyCode.A))
         {
             transform.position = transform.position - transform.right * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            AttackTrue();
         }
     }
 }
