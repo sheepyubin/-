af72f98f65a74b72fb27b79b50a225f9d571e63b
the-glory-of-the-Sun <aenigo23gi@naver.com>
2024-01-15 20:25:44 +0900

    2401김민수
    망이 일반 공격까지 완성

--- Is-the-order-a-Yubin/Assets/Script/MinSu/Mang/Attack.cs
+++ Is-the-order-a-Yubin/Assets/Script/MinSu/Mang/Attack.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject bullte;
+    public Transform sPoint;
+    public float timeBetweenShots;
 
+    private float shotTime;
     void Start()
     {
         
@@ -14,6 +18,18 @@ public class Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 direction=Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position;
+        float angle=Mathf.Atan2(direction.y,direction.x)*Mathf.Rad2Deg;
+        Quaternion rotation=Quaternion.AngleAxis(angle-90,Vector3.forward);
+        transform.rotation = rotation;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Time.time > shotTime)
+            {
+                Instantiate(bullte,sPoint.position,Quaternion.AngleAxis(angle-90,Vector3.forward));
+                shotTime = Time.time + timeBetweenShots;
+            }
+        }
     }
 }
